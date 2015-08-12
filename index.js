44fcc0ad9a0b030d5bffa3411fc1f232595424a9
Peter Mellett <peter@mymedialab.co.uk>
2015-08-12 12:05:31 +0100

    only had all that other stuff to play around

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-require("babel/register");
+var robot = require("robotjs");
 
-var app = require('./lib/app');
+function randomInt(low, high) {
+    return Math.floor(Math.random() * (high - low) + low);
+}
 
-app();
+//Get the mouse position, returns an object with x and y.
+var mouse  = robot.getMousePos();
+var screen = robot.getScreenSize();
+
+console.log("THERE'S A MOOSE IN THE HOOSE");
+
+for(;;) {
+  robot.moveMouseSmooth(randomInt(0, screen.width), randomInt(0, screen.height));
+}
